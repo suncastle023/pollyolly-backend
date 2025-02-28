2c67757083f71f35f6a7c1ae92c28c6f132fe443
suncastle023 <badabada0619@naver.com>
2025-03-01 01:06:21 +0900

    fix:custom 모델과 걸음 연결

--- steps/models.py
+++ steps/models.py
@@ -1,9 +1,9 @@
 # steps/models.py
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings  # ✅ 현재 사용 중인 사용자 모델 가져오기
 
 class StepCount(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)  # 사용자 정보
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)  # ✅ CustomUser 사용
     date = models.DateField(auto_now_add=True)  # 걸음 수 기록 날짜
     steps = models.IntegerField()  # 걸음 수 데이터
 
