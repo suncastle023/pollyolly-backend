fd2048b5061809370920aad447f3637a5f523040
suncastle023 <badabada0619@naver.com>
2025-03-02 22:02:31 +0900

    feat: 코인과 아이템을 관리할 수 있는 admin 제작

--- coin/admin.py
+++ coin/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from coin.models import Coin
 
-# Register your models here.
+@admin.register(Coin)
+class CoinAdmin(admin.ModelAdmin):
+    list_display = ("user", "amount", "last_rewarded_steps", "last_reward_date", "total_feed_bonus", "total_toy_bonus")
+    list_filter = ("last_reward_date",)
+    search_fields = ("user__email", "user__username")
+    readonly_fields = ("last_rewarded_steps", "last_reward_date", "total_feed_bonus", "total_toy_bonus")
+
+    # ✅ 슈퍼유저가 코인 수량을 조정할 수 있도록 설정
+    def get_readonly_fields(self, request, obj=None):
+        if request.user.is_superuser:
+            return []  # 슈퍼유저는 모든 필드를 수정 가능
+        return self.readonly_fields
--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Inventory
 
-# Register your models here.
+@admin.register(Inventory)
+class InventoryAdmin(admin.ModelAdmin):
+    list_display = ("user", "feed", "water", "toy", "last_fed", "last_water")
+    list_filter = ("last_fed", "last_water")
+    search_fields = ("user__email", "user__username")
+    readonly_fields = ("last_fed", "last_water")
+
+    # ✅ 관리자가 인벤토리 아이템 개수를 직접 수정할 수 있도록 설정
+    def get_readonly_fields(self, request, obj=None):
+        if request.user.is_superuser:
+            return []  # 슈퍼유저는 모든 필드를 수정 가능
+        return self.readonly_fields
+
+
