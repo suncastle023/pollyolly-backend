3b6926745292084f818351306c865a5777983fe9
suncastle023 <badabada0619@naver.com>
2025-03-04 21:50:03 +0900

    fix: admin  추가

--- store/admin.py
+++ store/admin.py
@@ -1,12 +1,5 @@
 from django.contrib import admin
 from .models import Item
-from inventory.models import Inventory
-
-@admin.register(Inventory)
-class InventoryAdmin(admin.ModelAdmin):
-    list_display = ("user", "feed", "water", "toy")  # ✅ 관리자에서 보이는 필드
-    search_fields = ("user__email",)  # ✅ 사용자 이메일 검색 가능
-    list_filter = ("user",)
 
 @admin.register(Item)
 class ItemAdmin(admin.ModelAdmin):
