d95370f1d58a1ecefd40d4a6a203aa9ff8c03e61
suncastle023 <badabada0619@naver.com>
2025-03-05 16:55:37 +0900

    feat: coin 개수 반환 api url

--- coin/urls.py
+++ coin/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import StepRewardAPIView
+from .views import StepRewardAPIView, get_user_coins
 
 app_name = 'coin'
 
 urlpatterns = [
     path("reward/", StepRewardAPIView.as_view(), name="step_reward"),
+    path("IhaveCoin/", get_user_coins, name="amount_of_coin"),
+
+
 ]
