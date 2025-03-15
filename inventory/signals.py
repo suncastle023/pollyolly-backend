51ebd60d7e86c3ac9d2c0c6cf7b440a3be8787dc
suncastle023 <badabada0619@naver.com>
2025-03-15 20:30:56 +0900

    fix: 기본 아이템  초기화 로직 추가

--- inventory/signals.py
+++ inventory/signals.py
@@ -6,4 +6,8 @@ from .models import Inventory
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_user_inventory(sender, instance, created, **kwargs):
     if created:
-        Inventory.objects.create(user=instance)
+        inventory = Inventory.objects.create(user=instance)
+        # 인벤토리 생성 시 기본으로 배경/집 아이템이 추가되는 것을 방지하기 위해
+        # 해당 ManyToManyField를 명시적으로 비워둡니다.
+        inventory.backgrounds.clear()
+        inventory.houses.clear()
